7a366dcc3b9cdc1f8e0db1c3eb8b7d373e655aea
Abroquah12 <abroquahslyvester12@gmail.com>
2025-07-31 22:20:34 +0000

    start up

---
+++ my-app/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main entry point of the application.</p>
+      <p>Feel free to explore the features and functionalities.</p>
+    </div>
+  )
+}
+
+export default HomePage
