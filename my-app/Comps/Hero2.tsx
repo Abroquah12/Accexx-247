143bd12d380a35a14a018029369515cc37dd04a1
Abroquah12 <abroquahslyvester12@gmail.com>
2025-08-01 00:24:35 +0000

    change

--- my-app/Comps/Hero2.tsx
+++ my-app/Comps/Hero2.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero2 = () => {
   return (
-    <div className='bg-blue-200 rounded-2xl shadow-lg p-5 flex flex-col space-y-6 relative bottom-20 right-20 z-10 w-[1050px] h-[160px] mx-auto mt-10 overflow-hidden'>
+    <div className='bg-white rounded-2xl shadow-md p-5 flex flex-col space-y-6 relative bottom-20 right-20 z-10 w-[1050px] h-[160px] mx-auto mt-10 overflow-hidden'>
         <h1 className="text-3xl font-bold text-gray-800"><u>General</u></h1>
         <div className='flex gap-4'>
             <div className='border border-gray-400 w-65 h-[58px]  px-6 py-2 rounded-2xl overflow-hidden'>
